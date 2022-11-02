71de38c16ecb95d5f6a3e33bc491a04c84d27649
Natureknight <vallez8244@gmail.com>
2022-11-02 12:18:22 +0200

    Improved logging

--- simplelock-core/src/main/java/com/simplelock/core/JdbcSimpleLockRunnableExecutor.java
+++ simplelock-core/src/main/java/com/simplelock/core/JdbcSimpleLockRunnableExecutor.java
@@ -24,6 +24,7 @@ package com.simplelock.core;
 import com.simplelock.api.LockRunnableExecutor;
 import com.simplelock.api.SimpleLock;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -35,6 +36,7 @@ import java.util.concurrent.TimeUnit;
  * @since 1.0.7
  */
 @RequiredArgsConstructor
+@Slf4j
 public class JdbcSimpleLockRunnableExecutor implements LockRunnableExecutor {
 
     private static final String UNIQUE_KEY = "unique-key-db37d712-c1e7-45b7-835c-f24b2a526fb9";
@@ -50,7 +52,22 @@ public class JdbcSimpleLockRunnableExecutor implements LockRunnableExecutor {
                 runnable.run();
             }
         } finally {
-            tokenOptional.ifPresent(token -> simpleLock.release(token, releaseAfter, timeUnit));
+            tokenOptional.ifPresent(token -> {
+                simpleLock.release(token, releaseAfter, timeUnit);
+
+                if (log.isWarnEnabled()) {
+                    long minutes = TimeUnit.MINUTES.convert(releaseAfter, timeUnit);
+                    // Print a log message here in case client wants to hold the lock for too long. This might end up
+                    // having the lock stuck in DB and never released (or released just after service restart only if
+                    // configured so), as there will be a scheduled thread spawned to execute the release in the
+                    // service node which acquired the lock initially
+                    if (minutes >= 1L) {
+                        log.warn("Holding a lock for too long might end up having your lock record stuck in database "
+                                + "and never released after e.g. service restart or crash. Currently you're about to "
+                                + "hold the lock for {} minute(s) or more.", minutes);
+                    }
+                }
+            });
         }
     }
 }
